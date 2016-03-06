349ac3d9f0abb0a95ca0e8da595d8812aab846f9
Jason Solano <jason.s@hotmail.es>
2016-03-06 00:44:14 -0600

    Juego

--- Juego de Memoria/main.swift
+++ Juego de Memoria/main.swift
@@ -4,9 +4,29 @@
 //
 //  Created by Jason Solano on 6/3/16.
 //  Copyright © 2016 Jason Solano. All rights reserved.
-//
 
-import Foundation
+// arreglo vacio
+var rango: [Int] = []
 
-print("Hello, World!")
+// Se llena el arreglo
+for i in 0...100{
+    rango.append(i)
+}
+
+//Operaciones
+
+for i in rango {
+    if i%5 == 0{
+        print("Bingo!!!      \(i) " )
+    }
+    if i%2 == 0{
+        print("par!!!        \(i) " )
+    }
+    if i%2 == 1{
+        print("impar!!!      \(i) " )
+    }
+    if i  >= 30 && i <= 40{
+        print("Viva Swift!!! \(i) " )
+    }
+}
 
